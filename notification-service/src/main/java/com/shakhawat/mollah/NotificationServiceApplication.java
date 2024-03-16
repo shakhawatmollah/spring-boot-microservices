37cd14199dd465b42f098c5a60a42e31be0ead9c
Shakhawat Mollah <sh.sumon99@gmail.com>
2024-03-16 20:23:34 +0600

    Monitoring with Promethuse and Grafana

--- notification-service/src/main/java/com/shakhawat/mollah/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/shakhawat/mollah/NotificationServiceApplication.java
@@ -1,6 +1,10 @@
 package com.shakhawat.mollah;
 
 import com.shakhawat.mollah.event.OrderPlacedEvent;
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,7 +12,11 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 @Slf4j
+@AllArgsConstructor
 public class NotificationServiceApplication {
+
+    private final ObservationRegistry observationRegistry;
+    private final Tracer tracer;
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationServiceApplication.class, args);
@@ -16,7 +24,11 @@ public class NotificationServiceApplication {
 
     @KafkaListener(topics = "notificationTopic")
     public void handleNotification(OrderPlacedEvent orderPlacedEvent){
-        log.info("Received Notification for Order - {}", orderPlacedEvent.getOrderNumber());
+        Observation.createNotStarted("on-message", this.observationRegistry).observe(() -> {
+            log.info("Got message <{}>", orderPlacedEvent);
+            log.info("TraceId- {}, Received Notification for Order - {}", this.tracer.currentSpan().context().traceId(),
+                    orderPlacedEvent.getOrderNumber());
+        });
     }
 
 }
